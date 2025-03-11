b2a4b5c531b86de8d9f293cd829138d34185e85a
Mitchell <mitch.heidbrink@gmail.com>
2025-03-11 14:44:17 -0400

    add friend request schema

---
+++ backend/src/models/friendRequestModel.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const friendRequestSchema = new mongoose.Schema({
+    snederId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        description: 'must be an ObjectId and is required'
+    },
+    receiverId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        description: 'must be an ObjectId and is required'
+    },
+    status: {
+        type: String,
+        enum: ['pending', 'accepted', 'rejected'],
+        default: 'pending',
+        description: 'must be a string and is required'
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        description: 'must be a date'
+    },
+    upDatedAt: {
+        type: Date,
+        default: Date.now,
+        description: 'must be a date'
+    }
+}, { collection: 'FriendRequest', versionKey: false });
+
+module.exports = mongoose.model('FriendRequest', friendRequestSchema);
